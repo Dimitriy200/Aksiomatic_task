a04b230becaf509048d822f95264cda6973160b0
Марченко Никита Игоревич <nikitamarchenko134@gmail.com>
2024-07-07 23:39:41 +0300

    add css

---
+++ src/main/java/com/axmtk_task/axmtk_task/config/MainSecurityConfig.java
@@ -0,0 +1,7 @@
+//package com.axmtk_task.axmtk_task.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class MainSecurityConfig extend WebSeq{
+//}
